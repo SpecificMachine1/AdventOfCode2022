691a76fae0e6e06f401d81585817efa6147b7dd3
fjt <fthibault1969@gmail.com>
2022-12-02 01:51:14 -0500

    finish second half of first day

--- day/day-1.scm
+++ day/day-1.scm
@@ -3,6 +3,7 @@
 ;; Copyright © 2022 Felix Thibault
 (import (scheme base)
 	(scheme file)
+	(srfi 1)
 	(srfi 115))
 
 ;; Port -> List of List of Numbers
@@ -21,4 +22,43 @@
 
 ;; [List-of [List-of Numbers]] -> Number
 (define (most-calories packs)
-  (let lp ((packs packs) )))
+  (let lp1 ((packs packs) (greatest 0))
+    (if (null? packs)
+        greatest
+	(let lp2 ((pack (car packs)) (sum 0))
+	  (if (null? pack)
+	    (if (> sum greatest)
+	      (lp1 (cdr packs) sum)
+	      (lp1 (cdr packs) greatest))
+	    (lp2 (cdr pack) (+ sum (car pack))))))))
+#|
+code for testing solution to first problem
+(most-calories
+  (get-data (open-input-file "../data/day1-example1.dat")))
+
+(most-calories
+  (get-data (open-input-file "../data/day1-input.dat")))
+|#
+
+;; [List-of [List-of Numbers]] -> Number
+(define (calories-of-top-three packs)
+  (let lp1 ((packs packs) (first 0) (second 0) (third 0))
+    (if (null? packs)
+      (list first second third)
+      (let lp2 ((pack (car packs)) (sum 0))
+	(if (null? pack)
+	  (cond
+	    ((> sum first) (lp1 (cdr packs) sum first second))
+	    ((> sum second) (lp1 (cdr packs) first sum second))
+	    ((> sum third) (lp1 (cdr packs) first second sum))
+	    (else (lp1 (cdr packs) first second third)))
+	  (lp2 (cdr pack) (+ sum (car pack))))))))
+
+#|
+code to test second solution
+(fold + 0  (calories-of-top-three
+  (get-data (open-input-file "../data/day1-example1.dat"))))
+
+(fold + 0  (calories-of-top-three
+  (get-data (open-input-file "../data/day1-input.dat"))))
+|#
